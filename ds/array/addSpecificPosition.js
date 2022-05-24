42e121ce9d0690142e4d386605e95266f2752815
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2022-05-24 11:27:18 -0300

    add element in specific position

---
+++ ds/array/addSpecificPosition.js
@@ -0,0 +1,14 @@
+/*
+ADICIONANDO ELEMENTOS DE UMA POSICAÇÃO ESPECÍFICA
+*/
+
+const elements = [1, 2, 3, 4, 5];
+
+/*
+  O primeiro argumento do método é o índice pelo qual queremos inserir o elemento.
+  O segundo argumento é a quantidade de elements que queremos remover (neste caso não iremos remover, por isso 0).
+  Do terceiro argumento em diante são os elementos que iremos adicionar a partir da posição especificada
+*/
+elements.splice(3, 0, 7,8,9);
+
+console.log(elements);
