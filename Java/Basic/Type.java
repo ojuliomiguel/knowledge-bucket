25601a37865d79f7d1d32499c0aff10734019c04
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-10-18 08:19:17 -0300

    Review data types

---
+++ Java/Basic/Type.java
@@ -0,0 +1,28 @@
+package Java.Basic;
+
+public class Type {
+  public static void main(String[] args) {
+
+    /*
+     * Java has two type of data:
+     * Primitive:
+     *  byte
+     *  short
+     *  int
+     *  long
+     *  float: 32-bit
+     *  double: 64-bit
+     *  char: 16-bit
+     *  boolean
+     * Object Types
+     *  String
+     *  List
+     *  Set
+     *  Map
+     * ...
+     */
+    
+    int amountOfMoney = 10;
+    
+  }
+}
