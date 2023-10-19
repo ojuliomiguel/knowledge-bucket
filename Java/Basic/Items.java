e0746836aa09ab3019fd526ffa404db64549243d
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-10-19 08:02:06 -0300

    items

---
+++ Java/Basic/Items.java
@@ -0,0 +1,24 @@
+package Java.Basic;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Items {
+  public static void main(String[] args) {
+
+    List<String> names = new ArrayList<>();
+    names.add("Júlio Miguel");
+    names.add("Amanda Lima");
+    names.add("Daniel Duarte");
+    names.add("Jeff");
+    names.add("Fernandeira Away");
+
+    for (String name : names) {
+      System.out.println(name);
+    }
+
+    //from java 8
+    names.forEach(name -> System.out.println(name));
+
+  }
+}
