0af2a3cee51212bc9ba53f6d834e8eec0d270763
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2023-10-30 08:32:33 -0300

    paths and query params

--- Java/Spring Boot/Fundamentals/solidcode/src/main/java/br/com/solidcode42/controller/FirstController.java
+++ Java/Spring Boot/Fundamentals/solidcode/src/main/java/br/com/solidcode42/controller/FirstController.java
@@ -1,16 +1,34 @@
 package br.com.solidcode42.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fool")
 public class FirstController {
-  
-  @GetMapping("info")
-  public String printInfo() {
-    return "Hello, this is a controller";
+
+  @GetMapping("info/{name}")
+  public String printInfo(@PathVariable String name) {
+    return "Hello, this is a controller,  %s".formatted(name);
+  }
+
+  @GetMapping("queryinfos")
+  public String queryInfo(@RequestParam String easterEgg) {
+    if (easterEgg != null) {
+      return "%s".formatted(easterEgg);
+    }
+
+    return "";
+  }
+
+  @GetMapping("allqueries")
+  public String queryInfo(@RequestParam Map<String, String> allParams) {
+    return "%s".formatted(allParams.entrySet());
   }
 
 }
