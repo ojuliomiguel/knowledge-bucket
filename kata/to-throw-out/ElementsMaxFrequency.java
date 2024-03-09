abd955032d67991318689f56a64024fcb0b9ae75
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2024-03-08 23:35:58 -0300

    kt: ElementsMaxFrequency [WIP]

---
+++ kata/to-throw-out/ElementsMaxFrequency.java
@@ -0,0 +1,14 @@
+public class ElementsMaxFrequency {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 9};
+        maxFrequency(nums);
+    }
+
+    public static int maxFrequency(int[] nums) {
+        for (int num : nums) {
+            System.out.println(num);
+        }
+
+        return 0;
+    }
+}
