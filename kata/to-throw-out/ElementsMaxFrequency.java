2a0c6f2c3855d4813df68fccc124261c03188fed
Júlio Miguel <37316841+ojuliomiguel@users.noreply.github.com>
2024-03-09 23:14:07 -0300

    kt: ElementsMaxFrequency [WIP]

--- kata/to-throw-out/ElementsMaxFrequency.java
+++ kata/to-throw-out/ElementsMaxFrequency.java
@@ -5,10 +5,45 @@ public class ElementsMaxFrequency {
     }
 
     public static int maxFrequency(int[] nums) {
-        for (int num : nums) {
-            System.out.println(num);
+       if (nums.length == 1) {
+            return 1;
         }
 
-        return 0;
+        ArrayList<Integer> maxFrequencies = new ArrayList<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 1;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    nums[i] = -1;
+                    count++;
+                }
+            }
+
+            maxFrequencies.add(count);
+        }
+
+        int max = Collections.max(maxFrequencies);
+
+        if (max == 1) {
+            return nums.length;
+        }
+
+        maxFrequencies.forEach(e -> System.out.println(e));
+        
+        int maxQtde = 0;
+        for (int k = 0; k < maxFrequencies.size(); k++) {
+            
+            if (maxFrequencies.get(k) == max) {
+                maxQtde++;
+            }
+        }
+
+        if (maxQtde != 0) {
+            max *= maxQtde;
+       }
+        System.out.println(max);
+        return max;
     }
 }
