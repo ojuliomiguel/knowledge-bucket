e5ffb7b4437185d7e20bfd252fffb2132175f2ab
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-10-23 23:20:21 -0300

    CrackingCode: ResizableArrays

---
+++ Java/DS/ResizableArrays.java
@@ -0,0 +1,11 @@
+void main() {
+    String[] words = {"apple", "banana", "cherry"};
+    String[] more = {"date", "elderberry"};
+
+    ArrayList<String> setence = new ArrayList<String>();
+
+    for (String w: words) setence.add(w);
+    for (String m: more) setence.add(m);
+
+    IO.println(setence);
+}
