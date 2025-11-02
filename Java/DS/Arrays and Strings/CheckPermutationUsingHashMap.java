c80febba4d6d876ea3fad8c186e6c61ec710e415
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-01 21:41:35 -0300

    Crk: Update CheckPermutation implementation to optimize HashMap usage and add test cases

--- Java/DS/Arrays and Strings/CheckPermutationUsingHashMap.java
+++ Java/DS/Arrays and Strings/CheckPermutationUsingHashMap.java
@@ -1,7 +1,9 @@
 import static java.lang.IO.println;
 
 void main() {
+    println(isPermutable("aab", "aba"));
     println(isPermutable("abb", "aba"));
+    println(isPermutable("bba", "abb"));
 }
 
 boolean isPermutable(String strOne, String strTwo) {
@@ -9,12 +11,23 @@ boolean isPermutable(String strOne, String strTwo) {
         return false;
     }
 
-    Map<Character, Integer> integerCharacterHashMap = new HashMap<Character, Integer>();
+    Map<Character, Integer> charsFrequenceMap = new HashMap<Character, Integer>();
 
-    char[] charStrOne = strOne.toCharArray();
-    char[] charStrTwo = strTwo.toCharArray();
+    for (int i = 0; i < strOne.length(); i++) {
+        charsFrequenceMap.merge(strOne.charAt(i), 1, Integer::sum);
+    }
 
-
+    for (int i = 0; i < strTwo.length(); i++) {
+        Integer frequency = charsFrequenceMap.get(strTwo.charAt(i));
+        if (frequency == null || frequency == 0) {
+            return false;
+        }
+        if (frequency == 1) {
+            charsFrequenceMap.remove(strTwo.charAt(i));
+        } else {
+            charsFrequenceMap.put(strTwo.charAt(i), frequency - 1);
+        }
+    }
 
     return true;
 }
