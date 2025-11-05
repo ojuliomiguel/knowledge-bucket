146481e1c97f78276a44190b3e4fecbe798d4d54
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-04 23:15:13 -0300

    Crk[WIP]: Add URLify implementation with space replacement logic

---
+++ Java/DS/Arrays and Strings/URLify.java
@@ -0,0 +1,34 @@
+import static java.lang.IO.println;
+
+void main() {
+    println(replaceSpaces("                Ju                                   lio Miguel     ".toCharArray(), 68));
+}
+
+String replaceSpaces(char[] str, int trueLength) {
+    final int CHARS_QTY = 3;
+    int spaceCount = 0, index;
+    for (int i = 0; i < trueLength; i++) {
+        if (str[i] == ' ') {
+            spaceCount++;
+        }
+    }
+
+    index = trueLength + spaceCount *2;
+    if (trueLength < str.length) {
+        str[trueLength] = '\0';
+    }
+    for (int i = trueLength -1; i >= 0; i--) {
+        if (str[i] == ' ') {
+            str[index -1] = '0';
+            str[index -2] = '2';
+            str[index -3] = '%';
+            index -= CHARS_QTY;
+        } else {
+            str[index -1] = str[i];
+            index--;
+        }
+    }
+    return new String(str);
+}
+
+
