278a714f5c037afba2ad83b8f4699328fdce3eb6
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-05 22:50:57 -0300

    Crk: Update URLify to simplify input and fix spacing inconsistencies in replaceSpaces

--- Java/DS/Arrays and Strings/URLify.java
+++ Java/DS/Arrays and Strings/URLify.java
@@ -1,7 +1,7 @@
 import static java.lang.IO.println;
 
 void main() {
-    println(replaceSpaces("                Ju                                   lio Miguel     ".toCharArray(), 68));
+    println(replaceSpaces("Julio Miguel     ".toCharArray(), 12));
 }
 
 String replaceSpaces(char[] str, int trueLength) {
@@ -12,19 +12,18 @@ String replaceSpaces(char[] str, int trueLength) {
             spaceCount++;
         }
     }
-
     index = trueLength + spaceCount *2;
     if (trueLength < str.length) {
         str[trueLength] = '\0';
     }
     for (int i = trueLength -1; i >= 0; i--) {
         if (str[i] == ' ') {
-            str[index -1] = '0';
-            str[index -2] = '2';
-            str[index -3] = '%';
+            str[index - 1] = '0';
+            str[index - 2] = '2';
+            str[index - 3] = '%';
             index -= CHARS_QTY;
         } else {
-            str[index -1] = str[i];
+            str[index - 1] = str[i];
             index--;
         }
     }
