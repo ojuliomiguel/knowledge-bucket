14e9d6c5d27d9a5ea13627c9b0937d22fc06f1e2
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-08 19:12:20 -0300

    Crk[WIP]: Update OneAway logic to use HashMap for character index tracking

--- Java/DS/Arrays and Strings/OneAway.java
+++ Java/DS/Arrays and Strings/OneAway.java
@@ -2,23 +2,27 @@ import static java.lang.IO.println;
 
 void main() {
     println(isOneAway("pale", "ple"));
+    println(isOneAway("pale", "paa"));
     println(isOneAway("pales", "pale"));
     println(isOneAway("pale", "bale"));
-    println(isOneAway("pale", "bake"));
+    println(isOneAway("pale", "bake")); // false
 }
 
 boolean isOneAway(String strOne, String strTwo) {
-    int counter = 0;
     if (strOne.equals(strTwo)) {
         return true;
     }
-    if (strOne.length() == strTwo.length() ) {
-        for (int i = 0; i < strTwo.length(); i++) {
-            if (strOne.charAt(i) != strTwo.charAt(i)) {
-                counter++;
-            }
+    HashMap<Character, Integer> charMap = new HashMap<>();
+
+    for (int i = 0; i < strOne.length(); i++) {
+        charMap.put(strOne.charAt(i), i);
+    }
+
+    for (int i = 0; i < strTwo.length(); i++) {
+        if (!charMap.containsKey(strTwo.charAt(i))) {
+            Integer idx = charMap.get(strTwo.charAt(i));
+
         }
 
     }
-    return counter <= 1;
 }
