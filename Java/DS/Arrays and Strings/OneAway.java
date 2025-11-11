9209a666cfa59cd504115492b31fc2e9cc31084a
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-10 23:17:49 -0300

    Crk[WIP]: Add oneEditInsert helper method to support OneAway logic refinement

--- Java/DS/Arrays and Strings/OneAway.java
+++ Java/DS/Arrays and Strings/OneAway.java
@@ -26,3 +26,21 @@ boolean isOneAway(String strOne, String strTwo) {
 
     }
 }
+
+boolean oneEditInsert(String strOne, String strTwo) {
+    int idx1 = 0;
+    int idx2 = 0;
+
+    while (idx2 < strTwo.length() && idx1 < strOne.length()) {
+        if (strOne.charAt(idx1) != strTwo.charAt(idx2)) {
+            if (idx1 != idx2) {
+                return false;
+            }
+            idx2++;
+        } else {
+            idx1++;
+            idx2++;
+        }
+    }
+    return true;
+}
