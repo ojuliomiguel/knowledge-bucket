64a4b41bd36b78a182adf9c83002b8db2f9229cf
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-11 23:11:30 -0300

    Crk: Add oneEditReplace helper method and enhance isOneAway logic with edit type checks

--- Java/DS/Arrays and Strings/OneAway.java
+++ Java/DS/Arrays and Strings/OneAway.java
@@ -9,7 +9,27 @@ void main() {
 }
 
 boolean isOneAway(String strOne, String strTwo) {
+    if (strOne.length() == strTwo.length()) {
+        return oneEditReplace(strOne, strTwo);
+    } else if (strOne.length() + 1 == strTwo.length()) {
+        return oneEditInsert(strOne, strTwo);
+    } else if (strOne.length() - 1 == strTwo.length()) {
+        return oneEditInsert(strTwo, strTwo);
+    }
+    return false;
+}
 
+boolean oneEditReplace(String strOne, String strTwo) {
+    boolean foundDiff = false;
+    for (int i = 0; i < strOne.length(); i++) {
+        if (strOne.charAt(i) != strTwo.charAt(i)) {
+            if (foundDiff) {
+                return false;
+            }
+            foundDiff = true;
+        }
+    }
+    return true;
 }
 
 boolean oneEditInsert(String strOne, String strTwo) {
