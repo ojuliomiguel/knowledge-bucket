0916db9cd09d71c5b75ce58599ea6a6c707f6e6f
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-15 23:49:38 -0300

    Crk: Introduce constant for first character to improve readability in StringCompression

--- Java/DSA/Arrays and Strings/StringCompressorFunctional.java
+++ Java/DSA/Arrays and Strings/StringCompressorFunctional.java
@@ -6,12 +6,13 @@ void main() {
 }
 
 String compression(String str) {
+    final int FIRST_CHAR = 0;
     if (str == null || str.isEmpty()) return str;
     Pattern pattern = Pattern.compile("(.)\\1*");
     Matcher matcher = pattern.matcher(str);
     String compressed = matcher.results()
             .map(MatchResult::group)
-            .map(group -> group.charAt(0) + String.valueOf(group.length()))
+            .map(group -> group.charAt(FIRST_CHAR) + String.valueOf(group.length()))
             .collect(Collectors.joining());
     return compressed.length() < str.length() ? compressed : str;
 }
