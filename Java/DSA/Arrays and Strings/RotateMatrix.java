01bdb7c5268a771909d1cc10442db5fb3c436c07
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-16 23:16:20 -0300

    Crk: Add RotateMatrix implementation to transpose matrix elements

---
+++ Java/DSA/Arrays and Strings/RotateMatrix.java
@@ -0,0 +1,17 @@
+import static java.lang.IO.println;
+
+void main() {
+
+}
+
+int[][] rotateClockwise(int[][] matrix) {
+    int matrixLength = matrix.length;
+    for (int i = 0; i < matrixLength; i++) {
+        for (int j = i + 1; j < matrixLength; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    return matrix;
+}
