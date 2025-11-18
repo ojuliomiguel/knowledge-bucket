3492530501cf08b9a41c157cc96fd479a08e3fed
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-11-17 23:13:16 -0300

    Crk: Add example usage of rotateClockwise with output demo in RotateMatrix

--- Java/DSA/Arrays and Strings/RotateMatrix.java
+++ Java/DSA/Arrays and Strings/RotateMatrix.java
@@ -1,6 +1,20 @@
 import static java.lang.IO.println;
 
 void main() {
+    int[][] matrixToRotate = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+
+    int[][] matrix = rotateClockwise(matrixToRotate);
+
+    for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[i].length; j++) {
+            println(matrix[i][j] + " ");
+        }
+        println();
+    }
 
 }
 
