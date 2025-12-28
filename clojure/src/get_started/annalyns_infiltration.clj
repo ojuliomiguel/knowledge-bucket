f797fcadb5b373ab44ace772cac7331905fc9726
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-12-27 22:38:32 -0300

    feat: Implement can-spy function and update knight, archer, and prisoner states

--- clojure/src/get_started/annalyns_infiltration.clj
+++ clojure/src/get_started/annalyns_infiltration.clj
@@ -6,3 +6,18 @@
    (not knight-awake?))
 
 (can-fast-attack? knight-awake?)
+
+(knight-awake? false)
+(def archer-awake? true)
+(def prisoner-awake? false)
+
+(defn can-spy? [knight-awake? archer-awake? prisoner-awake?] 
+  (or knight-awake? archer-awake? prisoner-awake?))
+
+(can-spy? knight-awake? archer-awake? prisoner-awake?)
+
+(archer-awake? false)
+(prisoner-awake? true)
+
+
+
