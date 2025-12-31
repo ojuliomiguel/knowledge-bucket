b5fede7248ab913f1cf003f23d40fdbf123ebda5
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2025-12-30 23:13:43 -0300

    fix: Refactor character awake states and update function calls for clarity

--- clojure/src/get_started/annalyns_infiltration.clj
+++ clojure/src/get_started/annalyns_infiltration.clj
@@ -7,33 +7,19 @@
 
 (can-fast-attack? knight-awake?)
 
-(def knight-awake? false)
-(def archer-awake? true)
-(def prisoner-awake? false)
-
 (defn can-spy? [knight-awake? archer-awake? prisoner-awake?] 
   (or knight-awake? archer-awake? prisoner-awake?))
 
-(can-spy? knight-awake? archer-awake? prisoner-awake?)
-
-;; It's necessary redefine "vars"
-(def archer-awake? false)
-(def prisoner-awake? true)
+(can-spy? true true true)
 
 (defn can-signal-prisoner? [archer-awake? prisoner-awake?] 
   (and (not archer-awake?) prisoner-awake?))
 
-(can-signal-prisoner? archer-awake? prisoner-awake?)
-
-(def knight-awake? false)
-(def archer-awake? true)
-(def prisoner-awake? false)
-(def dog-present? false)
+(can-signal-prisoner? false true)
 
 (defn can-free-prisoner? [knight-awake? archer-awake? prisoner-awake? dog-present?]
   (or
    (and dog-present? (not archer-awake?))
    (and (not dog-present?) prisoner-awake? (not knight-awake?) (not archer-awake?))))
 
-
-(can-free-prisoner? knight-awake? archer-awake? prisoner-awake? dog-present?)
+(can-free-prisoner? true false false true)
