946edb7df4a965a14c31584b1cd5f736cec3a6b7
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-01-04 23:04:14 -0300

    feat(cars-assemble): refine production-rate function logic

--- clojure/src/get_started/cars_assemble.clj
+++ clojure/src/get_started/cars_assemble.clj
@@ -2,8 +2,11 @@
 
 (defn production-rate
   [speed]
-  (cond (== speed 0) 0)
-  (cond (or (>= speed 1) (<= speed 4)) 221 * speed))
+  (cond
+    (== speed 0)                    0.0
+    (and (>= speed 1) (<= speed 4)) (double (* speed 221))
+    (and (>= speed 5) (<= speed 8)) (* speed 221 0.9)
+    (and (>= speed 9) (<= speed 10)) (* speed 221 0.77)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
