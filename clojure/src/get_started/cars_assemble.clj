049a6f37b13f145d16d13af76c2e14820bfe279e
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-01-05 22:59:54 -0300

    fix(cars-assemble): correct production-rate and working-items function logic

--- clojure/src/get_started/cars_assemble.clj
+++ clojure/src/get_started/cars_assemble.clj
@@ -1,15 +1,17 @@
 (ns get-started.cars-assemble)
 
-(defn production-rate
-  [speed]
+(defn production-rate [speed]
   (cond
     (== speed 0)                    0.0
     (and (>= speed 1) (<= speed 4)) (double (* speed 221))
     (and (>= speed 5) (<= speed 8)) (* speed 221 0.9)
-    (and (>= speed 9) (<= speed 10)) (* speed 221 0.77)))
+    (== speed 9) (* speed 221 0.8)
+    (== speed 10) (* speed 221 0.77)))
 
-(defn working-items
-  [speed]
+(defn working-items [speed]
   (cond
-    (and (>= speed 5) (<= speed 8) (int (* speed 221 0.9)))
-    ))
+    (== speed 0) 0
+    (and (>= speed 1) (<= speed 4)) (int (/ (* speed 221) 60))
+    (and (>= speed 5) (<= speed 8)) (int (/ (* speed 221 0.9) 60))
+    (== speed 9) (int (/ (* speed 221 0.8) 60))
+    (== speed 10) (int (/ (* speed 221 0.77) 60))))
