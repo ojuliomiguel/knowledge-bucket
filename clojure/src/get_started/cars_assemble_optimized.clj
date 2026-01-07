63e6a1f58044eb959c47b2763b15c01f8c76418d
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-01-06 23:12:35 -0300

    feat(cars-assemble): add optimized functions for production rate and working items

---
+++ clojure/src/get_started/cars_assemble_optimized.clj
@@ -0,0 +1,19 @@
+(ns get-started.cars-assemble-optimized)
+
+(def base-production-rate 221)
+
+(def success-rates
+  [{:max-speed 0 :rate 0.0}
+   {:max-speed 4 :rate 1.0}
+   {:max-speed 8 :rate 0.9}
+   {:max-speed 9 :rate 0.8}
+   {:max-speed 10 :rate 0.77}])
+
+(defn success-rate [speed]
+  (:rate (first (filter #(<= speed (:max-speed %)) success-rates))))
+
+(defn production-rate [speed]
+  (double (* speed base-production-rate (success-rate speed))))
+
+(defn working-items [speed]
+  (int (/ (production-rate speed) 60)))
