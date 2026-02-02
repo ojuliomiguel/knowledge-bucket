14d11277bbd53217c22af041a870f45e11983571
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-02-01 23:04:33 -0300

    feat(destructuring): add announce-treasure-location function for structured data handling

--- clojure/src/get_started/destructuring.clj
+++ clojure/src/get_started/destructuring.clj
@@ -8,7 +8,7 @@
 
 (get-first cats)
 
-(defn chooser 
+(defn chooser
   [[first, second, & choices]]
   (println (str "Your first choice is:" first))
   (println (str "Your second choice is" second))
@@ -17,3 +17,9 @@
 
 (chooser cats)
 
+(defn announce-treasure-location
+  [{lat :lat lgn :lgn}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure lgn:" lgn)))
+
+(announce-treasure-location {:lat 20.33 :lgn 33.21})
