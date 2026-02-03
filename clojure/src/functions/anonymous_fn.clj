521379e98ada6b072a4b4f32e6d591ce823613ed
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-02-02 22:44:32 -0300

    feat(anonymous-fn): add anonymous function examples and usage

---
+++ clojure/src/functions/anonymous_fn.clj
@@ -0,0 +1,18 @@
+(ns functions.anonymous-fn)
+
+; It's very similar to defn
+
+((fn [x] (* x 3)) 3)
+
+(map (fn [name] (str "Hi, " name)) 
+     ["Alfredo" "Ronaldo"])
+
+; We can bind a anonymous function to a name
+
+(def special-multiplier (fn [x] (* x 2)))
+
+(special-multiplier 2)
+
+; special way to declare
+
+(#(* 3 %) 3)
