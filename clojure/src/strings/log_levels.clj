bf753085c9549fdc6a6a00d11d3ef2e75f13fa55
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-02-10 23:03:23 -0300

    feat(log-levels): update message function to correctly extract log message from log line

--- clojure/src/strings/log_levels.clj
+++ clojure/src/strings/log_levels.clj
@@ -5,4 +5,13 @@
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
-  (let [[_ level message] (re-find #"\[(*.?\)]\s*(.*)")] s))
+  (str/trim (str/replace s "[ERROR]:" "")))
+
+(defn log-level
+  "Takes a string representing a log line
+   and returns its message with whitespace trimmed."
+  [s]
+  (let [[_ level _]
+        (re-find #"\[([A-Z]+)\s*(.*)]" s)] (str/lower-case level)))
+
+(println (message "[ERROR] Unexpected token"))
