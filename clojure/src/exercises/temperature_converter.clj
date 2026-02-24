5f710a0073a96dead9c863938ffe52fef8bd6a89
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-02-23 23:21:17 -0300

    feat(temperature-converter): fix variable name and improve temperature conversion functions

--- clojure/src/exercises/temperature_converter.clj
+++ clojure/src/exercises/temperature_converter.clj
@@ -1,9 +1,16 @@
 (ns exercises.temperature-converter)
 
+(def fahrenheit_constant 32)
 
 (defn celsius_to_fahrenheit
   [temperature]
-  (let [fahreneit_multiply 1.8]
-    (float (+ (* temperature fahreneit_multiply) 32))))
+  (let [fahrenheit_multiply 1.8]
+    (float (+ (* temperature fahrenheit_multiply) fahrenheit_constant))))
 
-println (celsius_to_fahrenheit 1)
+println (celsius_to_fahrenheit 0)
+
+(defn fahrenheit_to_celsius
+  [temperature]
+  (float (* (- temperature fahrenheit_constant) 5/9)))
+
+(println (fahrenheit_to_celsius 68))
