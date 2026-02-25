0c462290c27bb3d85d413988734ed80f7ac98faf
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-02-24 23:00:04 -0300

    feat(temperature-converter): add kelvin to celsius conversion function and fix print statement

--- clojure/src/exercises/temperature_converter.clj
+++ clojure/src/exercises/temperature_converter.clj
@@ -1,6 +1,7 @@
 (ns exercises.temperature-converter)
 
 (def fahrenheit_constant 32)
+(def kelvin_constant 273.15)
 
 (defn celsius_to_fahrenheit
   [temperature]
@@ -13,4 +14,10 @@ println (celsius_to_fahrenheit 0)
   [temperature]
   (float (* (- temperature fahrenheit_constant) 5/9)))
 
-(println (fahrenheit_to_celsius 68))
+(println (fahrenheit_to_celsius 212))
+
+(defn kelvin_to_celsius
+  [temperature]
+  (+ temperature kelvin_constant))
+
+(println (kelvin_to_celsius 0))
