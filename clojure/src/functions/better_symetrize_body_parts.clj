16924e7bd7d1418054dea4f514dd1163d778f07e
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-03-07 23:55:20 -0300

    feat(better-symetrize-body-parts): add better-symetrize-body-parts function implementation

---
+++ clojure/src/functions/better_symetrize_body_parts.clj
@@ -0,0 +1,8 @@
+(ns functions.better-symetrize-body-parts)
+
+(defn better-symetrize-body-parts
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
