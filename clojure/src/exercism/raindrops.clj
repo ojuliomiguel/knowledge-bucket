facbc03cbc9aff61fd0e99f83ef96df82e84f025
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2026-03-15 23:13:19 -0300

    feat(raindrops): enhance convert function to return number when not divisible by 3, 5, or 7

--- clojure/src/exercism/raindrops.clj
+++ clojure/src/exercism/raindrops.clj
@@ -9,6 +9,8 @@
   [num]
   (clojure.string/join (filter some? [(when (isDivisible? num 3) "Pling")
                                       (when (isDivisible? num 5) "Plang")
-                                      (when (isDivisible? num 7) "Plong")])))
+                                      (when (isDivisible? num 7) "Plong")
+                                      (when (not (or (isDivisible? num 3) (isDivisible? num 5) (isDivisible? num 7)))
+                                        num)])))
 
-(convert 30)
+(convert 34)
